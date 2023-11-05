95a78b963068dc9d99d8f4e2ba3cd8ab60e354fd
albertoisraelvazquezcasado <A23albertovc@iessanclemente.net>
2023-11-05 21:58:45 +0100

    Primer ejercicio de la tarea 2

--- tablaMultiplicardel9.js
+++ tablaMultiplicardel9.js
@@ -2,7 +2,7 @@ function TablaMultiplicar (j){
     document.write("<h2>Tabla de multiplicar del " + j +"</h2");
     document.write ("<ul>");
     // i++ es igual que i=i+1;
-    for (i=1;i<=10;i++);
+    for (i=1;i<=10;i++){
     document.write("<li>");
     document.write(j +"x " + i + "= " + j * i);
     document.write("</li>");
