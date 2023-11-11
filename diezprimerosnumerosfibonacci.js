9e957cc88598a71f7a7c31700b9c1c421576c81e
albertoisraelvazquezcasado <A23albertovc@iessanclemente.net>
2023-11-11 19:19:39 +0100

    segundo exercicio da tarefa 2

--- diezprimerosnumerosfibonacci.js
+++ diezprimerosnumerosfibonacci.js
@@ -1,14 +1,17 @@
-function fibonacci(n) {
-   if (n <=1) {
-    return n;
-   }
-   else {
-    return fibonacci(n-1) + fibonacci (n-2);
-   }
-function printFibonacciSecuencia(num) {
-    for (let i = 0; i < num; i++) {
-        console.log(fibonacci(i));
+function fib(position = 10, serie = []) {
+    if(serie.length === position)
+    return serie.pop()
+
+    if(serie.length === 0) {
+        serie.push(0)
     }
-}
-printFibonacciSecuencia(10);
+    else if(serie.length ===1) {
+        serie.push(1)
+    }
+    else {
+        const lastIndex = serie.length
+        const next = serie[lastIndex-2] + serie[lastIndex-1]
+        serie.push(next)
+    }
+    return fib(position,serie)
 }
