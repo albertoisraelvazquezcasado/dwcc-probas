b5b010fed63cfbaca0d41d003c161e567bc4bd9a
albertoisraelvazquezcasado <A23albertovc@iessanclemente.net>
2023-11-11 18:55:01 +0100

    Segundo exercicio da Tarefa 2

--- diezprimerosnumerosfibonacci.js
+++ diezprimerosnumerosfibonacci.js
@@ -1,14 +1,4 @@
 var fib = function(n) {
     if (n <=1) return n;
-
-    let prev2 = 0;
-    let prev1 = 1;
-    let c = 0;
-
-    for (let i = 2; i<=n; i++) {
-        c = prev1 + prev2;
-        prev2 = prev1;
-        prev1 = c;
-    }
-    return c;
+    return fib(n-1) + fib(n-2);
 }
