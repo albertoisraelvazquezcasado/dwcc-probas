14963ed423eabbc2c5a4e4d1975e256f461cf3ab
albertoisraelvazquezcasado <A23albertovc@iessanclemente.net>
2023-11-12 12:01:50 +0100

    javascript dos numeros primos

--- numeroprimoono.js
+++ numeroprimoono.js
@@ -1,20 +1,17 @@
-
-    function esPrimo(num) {
-
-    //Si el numero es 1 o menor, no es numero primo
-    if (num <=1) {
-        return false;
-    }
-    // Averiguar si número es divisible entre 2 y la raiz cuadrada del numero
-    for (var i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-            return false;
+const esPrimo = numero => {
+    if 
+    (numero == 0 || numero == 1 || numero == 4)
+    return false;
+    for (
+        let x = 2; x < numero / 2; x++){
+            if (numero % x == 0) return false;
         }
-    // Si el número no se puede dividir entre 2 y la raíz cuadrada, no primo
         return true;
 }
-//Vamos a visualizar el resultado por pantalla
-{
-    alert("El número 5 es primo: "+esPrimo(5));
-    alert("EL número 10 es primo: "+esPrimo(10));
+
+const numerosParaComprobar = [1,2,6,9,12,15];
+
+numerosParaComprobar.forEach(numero => {
+    console.log("¿%d es primo?%s", numero, esPrimo(numero));
 }
+    )
