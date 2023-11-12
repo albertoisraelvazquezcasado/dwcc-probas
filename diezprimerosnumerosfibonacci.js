f2b57127c235b05fe3af2693d07fbbc1ca5b76f9
albertoisraelvazquezcasado <A23albertovc@iessanclemente.net>
2023-11-12 17:29:57 +0100

    amosa os dez primeiros numeros fibonacci

--- diezprimerosnumerosfibonacci.js
+++ diezprimerosnumerosfibonacci.js
@@ -1,12 +1,19 @@
-function GenerarFibonnaci(numero) {
-var fibonacci = [];
-fibonacci[0] = 0;
-fibonacci[1] = 1;
-for (var i = 2; i < numero; i++) {
-    fibonacci[i] = fibonacci[i - 2] + fibonacci[i-1];
-
+var numero = Number(prompt("Mete un número entre 1 e 10"));
+if((numero >= 1) && (numero <= 10)) {
+    var x = 0;
+    var y = 1;
+    var z;
+    //Imprime o primeiro numero que será un 0 que non se pode meter
+    document.write(x + "<br>");
+//Bucle para imprimir o resto de números -1 que xa está impreso
+    for (i = 0; i < (numero-1); i ++) {
+        z = x + y;
+        document.write(z + "<br>");
+        y = x;
+        x = z;
+    }
 }
-    return fibonacci;
-}
-var f = GenerarFibonacci(10);
-console.log(f);
+    else {
+        alert("Número erróneo");
+    }
+    
