8fd66b0d2dd791fcfb3e41a90d52ec85d9a8430d
albertoisraelvazquezcasado <A23albertovc@iessanclemente.net>
2023-11-26 10:00:54 +0100

    contar elementos array

---
+++ Tarefa04/Array_Numero_Paises/arraynumeropaises.js
@@ -0,0 +1,9 @@
+
+
+
+  let paises = ["Afganistán", "Alemania", "Andorra","Argentina","Australia","Austria","Bélgica","Brasil","Canadá","China","Cuba","Dinamarca","Egipto","España","Estados Unidos","Finlandia","Francia","Grecia","Hungría","India","Irlanda","Italia","Japón","México","Noruega","Nueva Zelanda","Portugal","Suecia","Suiza","Turquía","Ucrania","Uruguay","Venezuela"];
+  
+  // Recorro el array para mostrar el número de elementos que tiene el array
+   //Se empieza por 0 siempre y usando el bucle for para recorrerlo
+   
+     console.log(paises.length);
